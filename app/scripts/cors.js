14bbd1fbf7cd7637cbf3820429721db7d222f454
Lucas Holmquist <lholmqui@redhat.com>
2012-12-12 09:21:04 -0500

    update to cors

--- app/scripts/cors.js
+++ app/scripts/cors.js
@@ -5,33 +5,17 @@ $( function() {
             cors = pipeline.add( {
                 name: "cors",
                 settings: {
-                    baseURL: "http://localhost:8080",
-                    endpoint: "/"
+                    baseURL: "http://localhost:8080/aerogear-controller-demo/",
+                    endpoint: "login/"
                 }
             });
 
             pipeline.pipes.cors.read({
                 success: function( data, xhr, thing1 ) {
                     console.log( data );
-
-                    pipeline.pipes.cors.remove({
-                        id: 1,
-                        title: "Another Created Task",
-                        date: "2012-07-13"
-                    },
-                    { success:function( data ) {
-                        console.log( data );
-                    },
-                    error: function( data ) {
-                        console.log( error );
-                    }
-                    });
                 },
                 error: function( error ) {
                     console.log( error );
-                },
-                jsonp: {
-                    callback: "jsonp"
                 }
             });
     }
